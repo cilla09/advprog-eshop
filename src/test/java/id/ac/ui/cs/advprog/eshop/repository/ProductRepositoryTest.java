a91f9a080cba8e7739a2b91f415ce89d56d61a22
cilla09 <priscilla.natanael@ui.ac.id>
2025-02-20 17:58:21 +0700

    fix issue ProductRepositoryTest.java

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -16,10 +16,6 @@ public class ProductRepositoryTest {
     @InjectMocks
     ProductRepository productRepository;
 
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
     void testCreateAndFind() {
         Product product = new Product();
